1d3583821fc241d50997dfc4e1f900924995c86d
Artur <artur389983@gmail.com>
2021-03-22 22:10:26 +0100

    add get json file with currencies rates as string

--- app/src/main/java/com/arturr300/currencyconverter/MainActivity.java
+++ app/src/main/java/com/arturr300/currencyconverter/MainActivity.java
@@ -3,11 +3,20 @@ package com.arturr300.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,10 +27,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnClear;
     Button btnConvert;
 
+
+
     //other variables
-    final float USD2PLN = (1.0f/3.89f);
-    final float USD2EUR = (1.0f/0.84f);
-    final float EUR2PLN = (1.0f/4.62f);
+
+
+
+    final float USD2PLN = 3.89f;
+    final float USD2EUR = 0.84f;
+    final float EUR2PLN = 4.62f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //assignment of listeners
         btnClear.setOnClickListener(this);
         btnConvert.setOnClickListener(this);
+
+        //thread responsibility fuses override
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
     @Override
@@ -48,30 +66,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 etUSD.setText("");
                 etEUR.setText("");
                 etPLN.setText("");
+                getCurrencyRateFromAPI("USD", "");
                 break;
             case R.id.buttonConvert:
 
                 if(etUSD.getText().toString().trim().length()>0)
                 {
                     float USD = Float.parseFloat(etUSD.getText().toString());
-                    float PLN = USD * (1.0f/USD2PLN);
-                    float EUR = USD * (1.0f/USD2EUR);
+                    float PLN = USD * USD2PLN;
+                    float EUR = USD * USD2EUR;
                     etPLN.setText(Float.toString(PLN));
                     etEUR.setText(Float.toString(EUR));
                 }
                 else if(etEUR.getText().toString().trim().length()>0)
             {
                 float EUR = Float.parseFloat(etEUR.getText().toString());
-                float PLN = EUR * (1.0f/EUR2PLN);
-                float USD = EUR * (USD2EUR);
+                float PLN = EUR * EUR2PLN;
+                float USD = EUR * (1.0f/USD2EUR);
                 etUSD.setText(Float.toString(USD));
                 etPLN.setText(Float.toString(PLN));
             }
                 else if(etPLN.getText().toString().trim().length()>0)
                 {
                     float PLN = Float.parseFloat(etPLN.getText().toString());
-                    float EUR = PLN * (EUR2PLN);
-                    float USD = PLN * (USD2PLN);
+                    float EUR = PLN * (1.0f/EUR2PLN);
+                    float USD = PLN * (1.0f/USD2PLN);
                     etUSD.setText(Float.toString(USD));
                     etEUR.setText(Float.toString(EUR));
                 }
@@ -82,5 +101,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 break;
         }
+    }
+
+    float getCurrencyRateFromAPI(String base, String target)
+    {
+        String url1 = "https://api.exchangeratesapi.io/latest?base=" + base;
+        String jsonStr = "";
+        try {
+            URL url2 = new URL(url1);
+            BufferedReader br = new BufferedReader(new InputStreamReader(url2.openStream()));
+            String output="";
+            String line="";
+            while((line=br.readLine())!=null)
+            {
+                output+=line;
+            }
+            Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
+
+        }
+        catch(IOException e)
+        {
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+        return 0.0f;
     }
 }
