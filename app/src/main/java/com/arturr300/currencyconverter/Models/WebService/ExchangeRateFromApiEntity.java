b45f640671f17903bad38334bf41646ad23fa37d
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-03-23 22:32:52 +0100

    create webservice, data from api entity

--- app/src/main/java/com/arturr300/currencyconverter/Models/WebService/ExchangeRateFromApiEntity.java
+++ app/src/main/java/com/arturr300/currencyconverter/Models/WebService/ExchangeRateFromApiEntity.java
@@ -1,4 +1,53 @@
+
 package com.arturr300.currencyconverter.Models.WebService;
 
+import javax.annotation.Generated;
+
+import com.google.gson.annotations.Expose;
+
+@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
 public class ExchangeRateFromApiEntity {
+
+    @Expose
+    private Double amount;
+    @Expose
+    private String base;
+    @Expose
+    private String date;
+    @Expose
+    private Rates rates;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Rates getRates() {
+        return rates;
+    }
+
+    public void setRates(Rates rates) {
+        this.rates = rates;
+    }
+
 }
---
+++ app/src/main/java/com/arturr300/currencyconverter/Models/WebService/Rates.java
@@ -0,0 +1,322 @@
+
+package com.arturr300.currencyconverter.Models.WebService;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class Rates {
+
+    @SerializedName("AUD")
+    private Double aUD;
+    @SerializedName("BGN")
+    private Double bGN;
+    @SerializedName("BRL")
+    private Double bRL;
+    @SerializedName("CAD")
+    private Double cAD;
+    @SerializedName("CHF")
+    private Double cHF;
+    @SerializedName("CNY")
+    private Double cNY;
+    @SerializedName("CZK")
+    private Double cZK;
+    @SerializedName("DKK")
+    private Double dKK;
+    @SerializedName("GBP")
+    private Double gBP;
+    @SerializedName("HKD")
+    private Double hKD;
+    @SerializedName("HRK")
+    private Double hRK;
+    @SerializedName("HUF")
+    private Double hUF;
+    @SerializedName("IDR")
+    private Long iDR;
+    @SerializedName("ILS")
+    private Double iLS;
+    @SerializedName("INR")
+    private Double iNR;
+    @SerializedName("ISK")
+    private Double iSK;
+    @SerializedName("JPY")
+    private Double jPY;
+    @SerializedName("KRW")
+    private Double kRW;
+    @SerializedName("MXN")
+    private Double mXN;
+    @SerializedName("MYR")
+    private Double mYR;
+    @SerializedName("NOK")
+    private Double nOK;
+    @SerializedName("NZD")
+    private Double nZD;
+    @SerializedName("PHP")
+    private Double pHP;
+    @SerializedName("PLN")
+    private Double pLN;
+    @SerializedName("RON")
+    private Double rON;
+    @SerializedName("SEK")
+    private Double sEK;
+    @SerializedName("SGD")
+    private Double sGD;
+    @SerializedName("THB")
+    private Double tHB;
+    @SerializedName("TRY")
+    private Double tRY;
+    @SerializedName("USD")
+    private Double uSD;
+    @SerializedName("ZAR")
+    private Double zAR;
+
+    public Double getAUD() {
+        return aUD;
+    }
+
+    public void setAUD(Double aUD) {
+        this.aUD = aUD;
+    }
+
+    public Double getBGN() {
+        return bGN;
+    }
+
+    public void setBGN(Double bGN) {
+        this.bGN = bGN;
+    }
+
+    public Double getBRL() {
+        return bRL;
+    }
+
+    public void setBRL(Double bRL) {
+        this.bRL = bRL;
+    }
+
+    public Double getCAD() {
+        return cAD;
+    }
+
+    public void setCAD(Double cAD) {
+        this.cAD = cAD;
+    }
+
+    public Double getCHF() {
+        return cHF;
+    }
+
+    public void setCHF(Double cHF) {
+        this.cHF = cHF;
+    }
+
+    public Double getCNY() {
+        return cNY;
+    }
+
+    public void setCNY(Double cNY) {
+        this.cNY = cNY;
+    }
+
+    public Double getCZK() {
+        return cZK;
+    }
+
+    public void setCZK(Double cZK) {
+        this.cZK = cZK;
+    }
+
+    public Double getDKK() {
+        return dKK;
+    }
+
+    public void setDKK(Double dKK) {
+        this.dKK = dKK;
+    }
+
+    public Double getGBP() {
+        return gBP;
+    }
+
+    public void setGBP(Double gBP) {
+        this.gBP = gBP;
+    }
+
+    public Double getHKD() {
+        return hKD;
+    }
+
+    public void setHKD(Double hKD) {
+        this.hKD = hKD;
+    }
+
+    public Double getHRK() {
+        return hRK;
+    }
+
+    public void setHRK(Double hRK) {
+        this.hRK = hRK;
+    }
+
+    public Double getHUF() {
+        return hUF;
+    }
+
+    public void setHUF(Double hUF) {
+        this.hUF = hUF;
+    }
+
+    public Long getIDR() {
+        return iDR;
+    }
+
+    public void setIDR(Long iDR) {
+        this.iDR = iDR;
+    }
+
+    public Double getILS() {
+        return iLS;
+    }
+
+    public void setILS(Double iLS) {
+        this.iLS = iLS;
+    }
+
+    public Double getINR() {
+        return iNR;
+    }
+
+    public void setINR(Double iNR) {
+        this.iNR = iNR;
+    }
+
+    public Double getISK() {
+        return iSK;
+    }
+
+    public void setISK(Double iSK) {
+        this.iSK = iSK;
+    }
+
+    public Double getJPY() {
+        return jPY;
+    }
+
+    public void setJPY(Double jPY) {
+        this.jPY = jPY;
+    }
+
+    public Double getKRW() {
+        return kRW;
+    }
+
+    public void setKRW(Double kRW) {
+        this.kRW = kRW;
+    }
+
+    public Double getMXN() {
+        return mXN;
+    }
+
+    public void setMXN(Double mXN) {
+        this.mXN = mXN;
+    }
+
+    public Double getMYR() {
+        return mYR;
+    }
+
+    public void setMYR(Double mYR) {
+        this.mYR = mYR;
+    }
+
+    public Double getNOK() {
+        return nOK;
+    }
+
+    public void setNOK(Double nOK) {
+        this.nOK = nOK;
+    }
+
+    public Double getNZD() {
+        return nZD;
+    }
+
+    public void setNZD(Double nZD) {
+        this.nZD = nZD;
+    }
+
+    public Double getPHP() {
+        return pHP;
+    }
+
+    public void setPHP(Double pHP) {
+        this.pHP = pHP;
+    }
+
+    public Double getPLN() {
+        return pLN;
+    }
+
+    public void setPLN(Double pLN) {
+        this.pLN = pLN;
+    }
+
+    public Double getRON() {
+        return rON;
+    }
+
+    public void setRON(Double rON) {
+        this.rON = rON;
+    }
+
+    public Double getSEK() {
+        return sEK;
+    }
+
+    public void setSEK(Double sEK) {
+        this.sEK = sEK;
+    }
+
+    public Double getSGD() {
+        return sGD;
+    }
+
+    public void setSGD(Double sGD) {
+        this.sGD = sGD;
+    }
+
+    public Double getTHB() {
+        return tHB;
+    }
+
+    public void setTHB(Double tHB) {
+        this.tHB = tHB;
+    }
+
+    public Double getTRY() {
+        return tRY;
+    }
+
+    public void setTRY(Double tRY) {
+        this.tRY = tRY;
+    }
+
+    public Double getUSD() {
+        return uSD;
+    }
+
+    public void setUSD(Double uSD) {
+        this.uSD = uSD;
+    }
+
+    public Double getZAR() {
+        return zAR;
+    }
+
+    public void setZAR(Double zAR) {
+        this.zAR = zAR;
+    }
+
+}
