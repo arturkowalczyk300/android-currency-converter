8bf766f37ca7eef9624659ff4fdf0cfbb2d573e6
arturkowalczyk300 <arturkowalczyk300@gmail.com>
2022-11-20 13:45:03 +0100

    changes in AllCurrenciesFragment: 	- use ConstraintLayout 	- use fragments for source and target currency section

---
+++ app/src/main/java/com/arturr300/currencyconverter/Views/TargetCurrencyFragment.java
@@ -0,0 +1,27 @@
+package com.arturr300.currencyconverter.Views;
+
+import static androidx.fragment.app.FragmentKt.setFragmentResult;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TargetCurrencyFragment extends SourceTargetCurrencyFragment {
+    public static final String FRAGMENT_REQUEST_KEY = "com.arturr300.currencyconverter.FRAGMENT_REQUEST_KEY_TARGET";
+    public static final String BUNDLE_VIEW_CREATED = "com.arturr300.currencyconverter.BUNDLE_VIEW_CREATED_TARGET";
+
+    public TargetCurrencyFragment() {
+        super(INPUT_TYPE.TARGET);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(this.BUNDLE_VIEW_CREATED, true);
+
+        setFragmentResult(this, FRAGMENT_REQUEST_KEY, bundle);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
